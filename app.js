2d5089c14a54d56239e083f6faa67167b5450a61
sunnysharma172 <kumarsharmasunny371@gmail.com>
2025-03-04 21:03:29 +0530

    Update app.js
    check out

--- app.js
+++ app.js
@@ -31,30 +31,23 @@ const showWinner = (userWin, userChoice, compChoice) => {
     msg.style.backgroundColor = "red";
   }
 };
-
 const playGame = (userChoice) => {
-  //Generate computer choice
   const compChoice = genCompChoice();
 
   if (userChoice === compChoice) {
-    //Draw Game
     drawGame();
   } else {
     let userWin = true;
     if (userChoice === "rock") {
-      //scissors, paper
       userWin = compChoice === "paper" ? false : true;
     } else if (userChoice === "paper") {
-      //rock, scissors
       userWin = compChoice === "scissors" ? false : true;
     } else {
-      //rock, paper
       userWin = compChoice === "rock" ? false : true;
     }
     showWinner(userWin, userChoice, compChoice);
   }
 };
-
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
